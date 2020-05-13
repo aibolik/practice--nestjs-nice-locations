80cb5a770546a2b3321fa879ccb87e07d2a9dd8d
Aibol Kussain <aibolikdev@gmail.com>
2020-05-13 07:45:25 +0200

    feat: create new endpoint and change unit and e2e tests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const APP_DIRECTORY = resolve(__dirname, '..');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+
   await app.listen(3000);
 }
 bootstrap();
